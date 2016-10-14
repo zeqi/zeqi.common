41073de1a272ab7e52c486ddcd769d3c08e42e19
zhuxijun <zhuxijun@b2cf.cn>
2016-10-14 16:29:01 +0800

    add some files

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zeqi
+ *         File: index
+ * ============Author info===========
+ *       Author: Xijun Zhu
+ *         Date: 16-10-14
+ *       Wechat: zhuzeqi2010
+ *           QQ: 304566647
+ * Office email: zhuxijun@b2cf.cn
+ * Author email: zhuzeqi2010@163.com
+ * ==========Author info end=========
+ */
+
+
+var moduleName = 'zeqi.common';
+console.log(moduleName);
+module.exports.moduleName = moduleName;
