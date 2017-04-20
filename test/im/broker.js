505d2ccf4fcebd3b0d773a239897337caaef4e42
zhuxijun <zhuxijun@b2cf.cn>
2017-04-20 12:02:18 +0800

    update im module

--- test/im/broker.js
+++ test/im/broker.js
@@ -4,7 +4,7 @@ var kafka = require('kafka-node');
 var common = require('../../index');
 var Producer = common.IM.Producer;
 var Consumer = common.IM.Consumer;
-var client = new kafka.Client('127.0.0.1:2181');
+/*var client = new kafka.Client('127.0.0.1:2181');
 
 var topic = 'tianjing';
 
@@ -13,20 +13,32 @@ function onMessage(message) {
 }
 
 var producer = Producer.init(client);
-var consumer = Consumer.init(client, topic, onMessage);
+var consumer = Consumer.init(client, topic, onMessage, {
+  groupId: topic,
+  autoCommit: true,
+  fetchMaxWaitMs: 1000,
+  fetchMaxBytes: 1024 * 1024
+});
 
-var data = { event: 'chat message', payload: { username: 'Test', message: '天地悠悠22222222222' } };
+var data = {event: 'chat message', payload: {username: 'Test', message: '天地悠悠22222222222'}};
 producer.sendMessagesByTopics(topic, data).then(data => {
-  data = { event: 'chat message', payload: { username: 'Test_1', message: '过客匆匆3333333333' } };
-  return producer.sendMessagesByTopics(topic, data).then(data => {
-    return data;
-  }).fail(err => {
-    console.log(err);
-    return err;
-  });
+  data = {event: 'chat message', payload: {username: 'Test_1', message: '过客匆匆3333333333'}};
+  return producer.sendMessagesByTopics(topic, data)
+}).then(data => {
+  return data;
 }).fail(err => {
   console.log(err);
   return err;
-});
+});*/
 
+var a = 0;
+console.log(++a);
+console.log(a);
 
+var b = 0;
+console.log(b++);
+console.log(b);
+
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
