4aca45c6c0f4a76efcd8321484114debda1ef04e
zhuxijun <zhuxijun@b2cf.cn>
2017-04-20 17:02:17 +0800

    add broker

--- test/im/broker.js
+++ test/im/broker.js
@@ -2,36 +2,41 @@
 
 var kafka = require('kafka-node');
 var common = require('../../index');
-var Producer = common.IM.Producer;
-var Consumer = common.IM.Consumer;
-/*var client = new kafka.Client('127.0.0.1:2181');
+/*var Producer = common.IM.ProducerFact;
+var Consumer = common.IM.ConsumerFact;*/
+var Broker = common.IM.Broker;
+// var client = new kafka.Client('127.0.0.1:2181');
 
 var topic = 'tianjing';
+var broker = Broker.init('zeqi.websocket');
+var producer = broker.producerFact;
+var consumer = broker.createConsumer(topic);
+
 
 function onMessage(message) {
   console.log('broker', message);
 }
 
-var producer = Producer.init(client);
-var consumer = Consumer.init(client, topic, onMessage, {
+/*var producer = Producer.init(client);
+var consumer = Consumer.init(client, topic, {
   groupId: topic,
   autoCommit: true,
   fetchMaxWaitMs: 1000,
   fetchMaxBytes: 1024 * 1024
-});
+}, onMessage);*/
 
-var data = {event: 'chat message', payload: {username: 'Test', message: '天地悠悠22222222222'}};
+var data = { event: 'chat message', payload: { username: 'Test', message: '天地悠悠22222222222' } };
 producer.sendMessagesByTopics(topic, data).then(data => {
-  data = {event: 'chat message', payload: {username: 'Test_1', message: '过客匆匆3333333333'}};
+  data = { event: 'chat message', payload: { username: 'Test_1', message: '过客匆匆3333333333' } };
   return producer.sendMessagesByTopics(topic, data)
 }).then(data => {
   return data;
 }).fail(err => {
   console.log(err);
   return err;
-});*/
+});
 
-var a = 0;
+/*var a = 0;
 console.log(++a);
 console.log(a);
 
@@ -41,4 +46,4 @@ console.log(b);
 
 for (var i = 0; i < 5; i++) {
   console.log(i);
-}
+}*/
